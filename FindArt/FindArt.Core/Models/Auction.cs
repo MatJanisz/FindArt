676e314d7631401fcce1592d3cd02b8452809266
MatJanisz <mateusz2716@gmail.com>
2021-03-25 19:52:06 +0100

    Added Auction

---
+++ FindArt/FindArt.Core/Models/Auction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindArt.Core.Models
+{
+	public class Auction
+	{
+		public string AuctionID { get; set; }
+		public decimal InitialPrice { get; set; }
+		public DateTime DueDate { get; set; }
+		public bool Active { get; set; }
+		public string ProductID { get; set; }
+		public Product Product { get; set; } //one to one
+	}
+}
---
+++ FindArt/FindArt.DataAccess/Migrations/20210322201557_AddProductType table.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FindArt.DataAccess.Migrations
+{
+    public partial class AddProductTypetable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductTypeID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    ProductTypeID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.ProductTypeID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypes",
+                columns: new[] { "ProductTypeID", "Name" },
+                values: new object[,]
+                {
+                    { 0, "Painting" },
+                    { 1, "Drawing" },
+                    { 2, "Graphics" },
+                    { 3, "Calligraphy" },
+                    { 4, "Fotography" },
+                    { 5, "Architecture" },
+                    { 6, "Sculpture" },
+                    { 7, "Ceramics" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypeID",
+                table: "Products",
+                column: "ProductTypeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeID",
+                table: "Products",
+                column: "ProductTypeID",
+                principalTable: "ProductTypes",
+                principalColumn: "ProductTypeID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeID",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductTypeID",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductTypeID",
+                table: "Products");
+        }
+    }
+}
